1cb9e6327ab1af41769e9ef1e8dd8ca2943bfee3
meanaa4y-cmyk <meanaa4y@gmail.com>
2026-08-04 21:54:09 +0500

    Add files via upload

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,25 +1,10 @@
-import { Link } from 'react-router-dom';
 import './footer.styles.scss';
-
-import githubIcon from "../../assets/github-mark.svg";
-import linkedinIcon from "../../assets/linkedin.svg";
 
 const FooterComponent = () => {
   return (
     <div className='container footer'>
       <div className='footer-content'>
         <span className='footer-brand'>MONARCH Clothing Co.</span>
-        <span className='footer-copy'>
-          2024 - React e-commerce demo site (<a href='https://github.com/fleps/clothing-store' className='underline' target='_blank' rel='noreferrer'>repo</a>) · <Link to='/admin'>Admin</Link>
-        </span>
-        <div className='footer-icons'>
-          <a href='https://www.linkedin.com/in/fleps/' target='_blank' rel='noreferrer'>
-            <img src={linkedinIcon} alt='LinkedIn' />
-          </a>
-          <a href='https://github.com/fleps' title='fleps github profile' target='_blank' rel='noreferrer'>
-            <img src={githubIcon} alt='Github' />
-          </a>
-        </div>
       </div>
     </div>
   );
